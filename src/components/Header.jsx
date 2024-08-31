eab28125a8c3c5b867fb5d6a393f412914553178
Deep Mankar <deepmankar0@gmail.com>
2024-08-31 14:09:17 +0530

    Modifying the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,25 +38,23 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="relative z-10 p-4 flex justify-between items-center bg-gradient-to-br from-gray-900 to-black bg-opacity-80">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 font-sans">
+    <header className="relative z-10 p-2 flex justify-between items-center bg-gradient-to-br from-gray-900 to-black bg-opacity-80">
+      <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500 font-sans">
         FilmSense
       </h1>
       {!isLoginPage && user && (
         <nav className="flex items-center">
-          <p className="text-white mr-4 text-sm md:text-base">
+          <p className="text-white mr-2 text-xs sm:text-sm truncate max-w-[100px] sm:max-w-none">
             <span className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-500">
-              Hello,{" "}
+              Hi,{" "}
             </span>
             <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-red-500">
               {user.displayName || "Guest"}
             </span>
-            <span className="text-yellow-400">!</span>
           </p>
-
           <button
             onClick={handleSignOut}
-            className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded text-sm md:text-base"
+            className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded text-xs sm:text-sm"
           >
             Sign Out
           </button>
